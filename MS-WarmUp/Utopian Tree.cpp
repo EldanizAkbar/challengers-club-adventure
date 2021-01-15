bc95d07a1020e08723699c9c39b5d59fcde85d20
Eldaniz Akbarzada <66679975+EldanizAkbar@users.noreply.github.com>
2021-01-15 10:45:37 +0400

    1

---
+++ MS-WarmUp/Utopian Tree.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the utopianTree function below.
+int utopianTree(int n) {
+    int m=1;
+ for (int i = 1; i <= n; i ++) {
+            if (i % 2 == 0) {
+                m ++;
+            } else {
+                m *= 2;
+            }
+        }
+
+    return m;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = utopianTree(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
